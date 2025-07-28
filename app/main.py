bec8bcc01ff8a77a87b19c0766acf8cd855ab64d
jaydrava <jraval.0901@gmail.com>
2025-07-28 02:04:23 -0400

    Initial commit: Project structure and venv setup

---
+++ app/main.py
@@ -0,0 +1,13 @@
+﻿from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from app.routes import users
+
+app = FastAPI()
+
+# Mount static and templates
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+# Include routes
+app.include_router(users.router)
---
+++ app/schemas.py
@@ -0,0 +1,17 @@
+﻿
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserRead(BaseModel):
+    id: int
+    email: EmailStr
+
+    class Config:
+        orm_mode = True
